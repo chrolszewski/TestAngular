81d5a9e6a7eebe6f03a847a6420534a7ab1faeec
Christian Olszewski <colsz@broadpark.no>
2016-10-08 02:20:12 +0200

    Part 4 of Angular2 tutorial https://angular.io/docs/ts/latest/tutorial/toh-pt4.html

---
+++ TestAngular/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+﻿import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Bob Baluba' },
+    { id: 12, name: 'Roy Arne Salater' },
+    { id: 13, name: 'Ahmed, the dead terrorist' },
+    { id: 14, name: 'Batman' },
+    { id: 15, name: 'Superman' },
+    { id: 16, name: 'Magneto' },
+    { id: 17, name: 'Wolverine' },
+    { id: 18, name: 'Spoderman' },
+    { id: 19, name: 'Pondus' },
+    { id: 20, name: 'Sherlock Holmes' }
+];
